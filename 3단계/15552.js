d017c9c3907b2e281628ed40770b3039908af1aa
hyejungan <138510303+hyejungan@users.noreply.github.com>
2024-03-07 17:59:06 +0900

    solved : 15552

---
+++ 3단계/15552.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+
+const env = process.platform === "linux" ? "dev/stdin" : "test.txt";
+
+let input = fs.readFileSync(env).toString().split("\n");
+
+const count = Number(input[0]);
+
+function solution(arr) {
+  let str = "";
+  for (let i = 1; i <= count; i++) {
+    const [a, b] = arr[i].split(" ");
+    str += Number(a) + Number(b) + "\n";
+  }
+  console.log(str);
+}
+
+solution(input);
