d275e326c7133ead5458fb8f19892b8ef3d448c1
hyejungan <138510303+hyejungan@users.noreply.github.com>
2024-03-07 18:39:06 +0900

    solved : 10952

---
+++ 3단계/10952.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+
+const env = process.platform === "linux" ? "dev/stdin" : "test.txt";
+
+let input = fs.readFileSync(env).toString().split('\n');
+
+function solution(arr) {
+  let str = '';
+  for(let i = 0; i < arr.length; i++) {
+    const [a, b] = arr[i].split(' ').map((n) => +n);
+    if(a <= 0 || b >= 10) break
+    str += `${a + b}` + '\n'
+  }
+  console.log(str)
+}
+
+solution(input)
