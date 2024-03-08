98d07ff6a06c46f02fbc91d19cda0f6387da7f78
hyejungan <138510303+hyejungan@users.noreply.github.com>
2024-03-08 18:32:56 +0900

    solved : 10810

---
+++ 4단계/10810.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+
+const env = process.platform === "linux" ? "dev/stdin" : "test.txt";
+
+let input = fs.readFileSync(env).toString().split("\n");
+
+const [N, M] = input[0].split(" ").map(Number);
+const baskets = new Array(N).fill(0);
+console.log(baskets);
+
+for (let i = 1; i <= M; i++) {
+  const [start, end, ball] = input[i].split(" ").map(Number);
+
+  for (let j = start; j <= end; j++) {
+    baskets[j - 1] = ball;
+  }
+}
+
+console.log(baskets.join(" "));
