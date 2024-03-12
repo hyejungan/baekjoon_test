b1089c89e2ad8a3fae3e022acc5e4caac6529cfc
hyejungan <138510303+hyejungan@users.noreply.github.com>
2024-03-12 11:55:00 +0900

    solved : 9086

--- 5단계/2743.js
+++ 5단계/2743.js
@@ -2,10 +2,14 @@ const fs = require("fs");
 
 const env = process.platform === "linux" ? "dev/stdin" : "test.txt";
 
-let input = fs.readFileSync(env).toString().trim();
+let input = fs.readFileSync(env).toString().trim().split("\n");
 
-function solution(str) {
-  console.log(str.length);
+function solution(n, string) {
+  let str = "";
+  for (let i = 0; i < n; i++) {
+    str += string[i][0] + string[i][string[i].length - 1] + "\n";
+  }
+  console.log(str);
 }
 
-solution(input);
+solution(+input[0], input.slice(1));
