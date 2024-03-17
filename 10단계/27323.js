f81fb656a90ff4d0ac6a186748a345f1790376ba
hyejungan <138510303+hyejungan@users.noreply.github.com>
2024-03-17 13:15:17 +0900

    solved : 27323

---
+++ 10단계/27323.js
@@ -0,0 +1,7 @@
+const fs = require("fs");
+
+const env = process.platform === "linux" ? "/dev/stdin" : "test.txt";
+
+let [M, N] = fs.readFileSync(env).toString().split("\n").map(Number);
+
+console.log(M * N)
