7d9e3f343db38c5c20b43693a19dbd4f4a280f9c
hyejungan <138510303+hyejungan@users.noreply.github.com>
2024-04-27 14:06:46 +0900

    solved : 10773

--- 스택_큐/10773.js
+++ 스택_큐/10773.js
@@ -6,17 +6,16 @@ let [N, ...arr] = fs
   .readFileSync(env)
   .toString()
   .trim()
-  .split("\n")
-  .map(Number);
+  .split("\n");
 
 let stack = [];
 
-for (let i = 0; i < N; i++) {
-  if (arr[i] === 0) {
+for (let i = 0; i < Number(N); i++) {
+  if (stack.length > 0 && arr[i] == 0) {
     stack.pop();
   } else {
     stack.push(arr[i]);
   }
 }
 
-console.log(stack.reduce((acc, cur) => acc + cur));
+console.log(stack.reduce((acc, cur) => Number(acc) + Number(cur), 0));
